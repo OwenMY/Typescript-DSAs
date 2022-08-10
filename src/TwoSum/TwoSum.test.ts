a58cad015824a0f13af81d978b714a3a71b59b98
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-10 00:09:47 -0700

    install and configure eslint

--- src/TwoSum/TwoSum.test.ts
+++ src/TwoSum/TwoSum.test.ts
@@ -1,16 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const func = require('./TwoSum');
 
 describe('TwoSum', () => {
   test('Should find indexes of two numbers within input array', () => {
     const nums: number[] = [1, 2, 3, 4, 5];
-    const target: number = 6;
+    const target = 6;
     const expected: number[] = func(nums, target);
 
     expect(expected).toEqual([0, 4]);
   });
   test('Should return empty array if no solution is found', () => {
     const nums: number[] = [0, 0, 0, 0, 0];
-    const target: number = 5;
+    const target = 5;
     const expected: number[] = func(nums, target);
 
     expect(expected).toEqual([]);
--- src/TwoSum/TwoSum.ts
+++ src/TwoSum/TwoSum.ts
@@ -1,8 +1,8 @@
 const twoSum = (nums: number[], target: number):number[] => {
   const indexes: number[] = [];
 
-  for (let i: number = 0; i < nums.length; i++) {
-    let differenceIndex: number = nums.lastIndexOf(target - nums[i]);
+  for (let i = 0; i < nums.length; i++) {
+    const differenceIndex: number = nums.lastIndexOf(target - nums[i]);
     if (differenceIndex > -1 && differenceIndex !== i) {
       indexes.push(i, differenceIndex);
       break;
