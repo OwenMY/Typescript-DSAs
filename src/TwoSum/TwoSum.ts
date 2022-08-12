777c327fd872b564f682269be6dac8cb0d2fdbf2
Owen Yoshishige <owenyoshishige@gmail.com>
2022-08-11 20:48:11 -0700

    configured prettier

--- src/TwoSum/TwoSum.ts
+++ src/TwoSum/TwoSum.ts
@@ -1,4 +1,4 @@
-const twoSum = (nums: number[], target: number):number[] => {
+const twoSum = (nums: number[], target: number): number[] => {
   const indexes: number[] = [];
 
   for (let i = 0; i < nums.length; i++) {
